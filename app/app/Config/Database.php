4988d089827d0d725dda22d99563796eca9df2a9
Felipe Ferreira <frisney@pm.me>
2021-11-07 20:13:19 -0300

    Reorganizacao

--- app/app/Config/Database.php
+++ app/app/Config/Database.php
@@ -32,11 +32,11 @@ class Database extends Config
      */
     public $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
+        'hostname' => 'db',
         'username' => '',
         'password' => '',
         'database' => '',
-        'DBDriver' => 'MySQLi',
+        'DBDriver' => 'Postgre',
         'DBPrefix' => '',
         'pConnect' => false,
         'DBDebug'  => (ENVIRONMENT !== 'production'),
@@ -47,7 +47,7 @@ class Database extends Config
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port'     => 3306,
+        'port'     => 0,
     ];
 
     /**
