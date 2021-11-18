d1550733d6393edce462273327dcb35cbdfcdd27
Felipe <frisney@pm.me>
2021-11-17 21:21:13 -0300

    Api Response Trait

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -6,10 +6,12 @@ use App\Controllers\BaseController;
 use App\Models\Endereco;
 use App\Models\Pessoa;
 use App\Models\Usuario;
+use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class UsuarioController extends BaseController
 {
+    use ResponseTrait;
     protected $usuario;
     protected $pessoa;
 
@@ -21,94 +23,64 @@ class UsuarioController extends BaseController
 
     public function new()
     {
-        if ($this->request->header('Content-Type')->getValue() != 'application/json'){
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_NOT_ACCEPTABLE)
-                ->send();
+        if (!$this->request->hasHeader('Content-Type') || $this->request->header('Content-Type')->getValue() != 'application/json'){
+            return $this->fail('Nao foi possivel salvar endereco', 406);
         }
 
         $data = $this->request->getJSON(true);
 
         $endereco = new Endereco();
         if(!$endereco->insert($data['endereco'])){
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
-                ->setBody('Nao foi possivel salvar endereco')
-                ->send();
+            return $this->fail('Nao foi possivel salvar endereco', 400);
         }
 
         $data['pessoa']['id_endereco'] = $endereco->getInsertID();
         $pessoa = new Pessoa();
         if(!$pessoa->insert( $data['pessoa'] ))
         {
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
-                ->setBody('Nao foi possivel salvar pessoa')
-                ->send();
+            return $this->fail('Nao foi possivel salvar pessoa', 400);
         }
 
         $data['usuario']['senha'] = password_hash($data['usuario']['senha'], PASSWORD_BCRYPT);
         $data['usuario']['id_pessoa'] = $pessoa->getInsertID();
         if(!$this->usuario->insert( $data['usuario'] ))
         {
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
-                ->setBody('Nao foi possivel salvar usuario')
-                ->send();
+            return $this->fail('Nao foi possivel salvar usuario', 400);
         }
 
-        return $this->response
-            ->setStatusCode(ResponseInterface::HTTP_OK)
-            ->setBody(null)
-            ->setJSON($data)
-            ->send();
+        return $this->respondCreated($data);
     }
 
     public function update()
     {
-        if ($this->request->header('Content-Type')->getValue() != 'application/json'){
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_NOT_ACCEPTABLE)
-                ->send();
+        if (!$this->request->hasHeader('Content-Type') || $this->request->header('Content-Type')->getValue() != 'application/json'){
+            return $this->fail('Nao foi possivel salvar endereco', 406);
         }
 
         $data = $this->request->getJSON(true);
 
         $endereco = new Endereco();
         if(!$endereco->update($data['endereco'])){
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
-                ->setBody('Nao foi possivel salvar endereco')
-                ->send();
+            return $this->fail('Nao foi possivel salvar endereco', 400);
         }
 
         $pessoa = new Pessoa();
         if(!$pessoa->update( $data['pessoa'] ))
         {
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
-                ->setBody('Nao foi possivel salvar pessoa')
-                ->send();
+            return $this->fail('Nao foi possivel salvar pessoa', 400);
         }
 
         $data['usuario']['senha'] = password_hash($data['usuario']['senha'], PASSWORD_BCRYPT);
         if(!$this->usuario->update( $data['usuario'] ))
         {
-            return $this->response
-                ->setStatusCode(ResponseInterface::HTTP_BAD_REQUEST)
-                ->setBody('Nao foi possivel salvar usuario')
-                ->send();
+            return $this->fail('Nao foi possivel salvar usuario', 400);
         }
 
-        return $this->response
-            ->setStatusCode(ResponseInterface::HTTP_OK)
-            ->setBody(null)
-            ->setJSON($data)
-            ->send();
+        return $this->respond($data, 200);
     }
 
-    public function delete()
-    {
-       $this->usuario->delete($this->request->get)
-    }
+//    public function delete()
+//    {
+//       $this->usuario->delete($this->request->get)
+//    }
 }
