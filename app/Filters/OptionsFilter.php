257e733116769a8f2c26d92d60ffdc2e9f7548e8
Felipe <frisney@pm.me>
2021-11-19 20:06:37 -0300

    correcao parametros padroes

--- app/Filters/OptionsFilter.php
+++ app/Filters/OptionsFilter.php
@@ -7,7 +7,7 @@ use CodeIgniter\Filters\FilterInterface;
 
 Class OptionsFilter implements FilterInterface
 {
-    public function before(RequestInterface $request)
+    public function before(RequestInterface $request, $arguments = null)
     {
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
@@ -18,5 +18,5 @@ Class OptionsFilter implements FilterInterface
         }
     }
 
-    public function after(RequestInterface $request, ResponseInterface $response) {}
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null){}
 }
